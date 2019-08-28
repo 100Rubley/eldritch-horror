0bc7c47ee309bb4678e0a65773163b405a0f66ba
Luffi2539 <vkkovalev91@gmail.com>
2019-08-28 17:23:21 +0300

    Updates

---
+++ precache-manifest.3cf861028f3f10044bee308eda58af8c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c5983ad378beecb425fcbeed061f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7743829c9f45a63f8f84",
+    "url": "/static/css/main.5a5d6517.chunk.css"
+  },
+  {
+    "revision": "1f93598ddc4ad0e7d611",
+    "url": "/static/js/2.2bbb29e2.chunk.js"
+  },
+  {
+    "revision": "7743829c9f45a63f8f84",
+    "url": "/static/js/main.62210d73.chunk.js"
+  },
+  {
+    "revision": "57c516cd233bc40a85af",
+    "url": "/static/js/runtime~main.19d33c78.js"
+  },
+  {
+    "revision": "319399e625ca4784e2bf3c8c2ebe32dd",
+    "url": "/static/media/Azathoth.319399e6.png"
+  },
+  {
+    "revision": "d5bd9a97071218906852ad312d65436b",
+    "url": "/static/media/Cthulthu.d5bd9a97.png"
+  },
+  {
+    "revision": "a5c56a09e641c2f607c657d756d72842",
+    "url": "/static/media/IogSothoth.a5c56a09.png"
+  },
+  {
+    "revision": "1d8956e66dcc767b6bbd89de0f47530b",
+    "url": "/static/media/ShubNiggurath.1d8956e6.png"
+  },
+  {
+    "revision": "f98095665d7f008113ddd4c93e2fc67f",
+    "url": "/static/media/home.f9809566.png"
+  }
+]);
